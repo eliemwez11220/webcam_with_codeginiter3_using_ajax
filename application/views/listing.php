9688013660c474db8ed57397e84a7192d0ddfea6
Elie Mwez Rubuz <57721489+eliemwez11220@users.noreply.github.com>
2021-03-30 15:31:35 +0200

    Ajout de liens de fichiers locaux dans la liste des images

--- application/views/listing.php
+++ application/views/listing.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Capture || Codeigniter</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-</head>
+	<title>Listing || Codeigniter</title>
+	<link rel="stylesheet" href="<?= base_url('global/css/'); ?>bootstrap.min.css">
+	</head>
 <body>
 	<div class="container">
 		<div class="row">
@@ -17,8 +17,8 @@
 						<div class="col-md-4 mb-3">
 							<div class="card text-center">
 					<div class="card-body">
-					
-						
+
+
 								<img src="<?= base_url('uploads/'.$value->image); ?>" style="width: 120px!important; height: 120px; border-radius: 100px">
 								<br>
 								<span class="text-center">
@@ -35,10 +35,11 @@
 		</div>
 	</div>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.js"></script>
+
+		<script src="<?= base_url('global/js/'); ?>jquery.min.js"></script>
+		<script src="<?= base_url('global/js/'); ?>popper.min.js"></script>
+		<script src="<?= base_url('global/js/'); ?>bootstrap.min.js"></script>
+		<script src="<?= base_url('global/js/'); ?>webcam.js"></script>
 
 </body>
 </html>
